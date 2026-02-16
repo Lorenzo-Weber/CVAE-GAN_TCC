eb09614eab0e7ad104634865966d59c9ff8b3c88
lorenzo <lcweber@inf.ufsm.br>
2026-02-15 21:28:22 -0300

    re-starting project

---
+++ main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Importing the beer ds i found on kaggle
+df = pd.read_csv('data/beer.csv')
+
+# First column is the label and the rest is the actual NIR data
+y = df.iloc[:, 0]
+x = df.iloc[:, 1:]
+
+
+x = np.linspace(0, 10, 100)
+y = 4 + 1 * np.sin(2 * x)
+x2 = np.linspace(0, 10, 25)
+y2 = 4 + 1 * np.sin(2 * x2)
+
+# plot
+fig, ax = plt.subplots()
+
+ax.plot(x2, y2 + 2.5, 'x', markeredgewidth=2)
+ax.plot(x, y, linewidth=2.0)
+ax.plot(x2, y2 - 2.5, 'o-', linewidth=2)
+
+ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+
+plt.show()
